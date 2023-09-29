7e1b024cdd9632922c50692ff32cf7ef75abe637
almuly <Alexander.Mulyarchik@solbeg.com>
2023-09-29 16:38:06 +0300

    create:add payment method

--- install/payselection_create_link/payselection_pay_link.php
+++ install/payselection_create_link/payselection_pay_link.php
@@ -3,6 +3,7 @@
 use Bitrix\Sale\Payment;
 use Bitrix\Sale\PaySystem\Manager;
 use Bitrix\Main\Application;
+use Bitrix\Main\Mail\Event;
 
 
 if (isset($_GET['Order_ID'])) {
@@ -11,27 +12,34 @@ if (isset($_GET['Order_ID'])) {
 
     \Bitrix\Main\Loader::includeModule("sale");
     $order = \Bitrix\Sale\Order::load((int)$_GET['Order_ID']);
+    $email = $order->getPropertyCollection()->getUserEmail()->getValue();
     $paymentCollection = $order->getPaymentCollection();
     $payment = $paymentCollection [0];
+
     $service = \Bitrix\Sale\PaySystem\Manager::getObjectById($payment->getPaymentSystemId());
     $context = \Bitrix\Main\Application::getInstance()->getContext();
-    $request2 = $service->initiatePay($payment, $context->getRequest());
-    $requestParams = $request2->getTemplateParams($payment);
 
-    $url = 'https://gw.payselection.com/webpayments/paylink_create';
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-    curl_setopt($ch, CURLOPT_USERPWD, "fLdy7fSenC6KVDKm");
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($requestParams));
-    $content = curl_exec($ch);
-    curl_close($ch);
-    $content = json_decode($content, true);
-    if ($content['Success'] == 'true') {
-        echo 'Ссылка на оплату отправлена клиенту!';
-    } else echo 'Что то пошло не так! Обратитесь к администратору сайта!';
+    $request = $service->initiatePay($payment, $context->getRequest()); ;
+
+
+    $eventName = 'SEND_PAYMENT_LINK';
+    $siteId = 's1';
+    $fields = array(
+        'EMAIL' => $email,
+        'PAYMENT_LINK' => $request,
+    );
+
+    $result = Event::send(array(
+        "EVENT_NAME" => $eventName,
+        "LID" => SITE_ID,
+        "C_FIELDS" => $fields,
+    ));
+
+    if ($result->isSuccess()) {
+        echo "Ссылка на оплату отправлена на адрес $email.";
+    } else {
+        echo "Ошибка при отправке ссылки на оплату.";
+    }
 
 };
 
