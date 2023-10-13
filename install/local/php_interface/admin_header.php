db3be086a8f7f46d4ff915f40b56a4978d621250
Alexander.Mulyarchik <alexander.mulyarchik@solbeg.com>
2023-10-13 15:53:25 +0300

    create: email sending successfully

--- install/local/php_interface/admin_header.php
+++ install/local/php_interface/admin_header.php
@@ -5,7 +5,7 @@
             type: 'GET',
             url: '/payselection_create_link/payselection_pay_link.php?Order_ID='+id,
             success: function(data) {
-                alert(data);
+                alert("Письмо отправлено успешно!");
             },
             error: function(xhr, str){
                 alert('Возникла ошибка: ' + xhr.responseCode);
--- install/payselection_create_link/payselection_pay_link.php
+++ install/payselection_create_link/payselection_pay_link.php
@@ -19,28 +19,74 @@ if (isset($_GET['Order_ID'])) {
     $service = \Bitrix\Sale\PaySystem\Manager::getObjectById($payment->getPaymentSystemId());
     $context = \Bitrix\Main\Application::getInstance()->getContext();
 
-    $request = $service->initiatePay($payment, $context->getRequest()); ;
+    $request = $service->initiatePay($payment, $context->getRequest());;
+    $paymentURL = $request->getPaymentUrl();
 
+    /**
+     * TODO this is better way, but it doesn't work locally
+     */
+//    $eventName = "SEND";
+//    $eventMessage = new CEventMessage;
+//    $arFields = array(
+//        "ACTIVE" => "Y",
+//        "EVENT_NAME" => $eventName,
+//        "LID" => "s1",
+//        "EMAIL_TO" => $email,
+//        "SUBJECT" => "Ссылка на оплату",
+//        "MESSAGE" => '
+//            <div class="mb-4">
+//                <p>Услугу предоставляет сервис <b>&laquo;Payselection&raquo;</b>.</p>
+//                <p>Сумма к оплате: </p>
+//                <div class="d-flex align-items-center mb-3">
+//                    <div class="col-auto pl-0">
+//                        <a class="btn btn-lg btn-success pl-4 pr-4" style="border-radius: 32px;" href="' . $paymentURL . '">Оплатить</a>
+//                    </div>
+//                    <div class="col pr-0">&nbsp;&nbsp;Вы будете перенаправлены на страницу оплаты</div>
+//                </div>
+//            </div>
+//        ',
+//        "BODY_TYPE" => "html"
+//    );
+//
+//    $eventMessage->Add($arFields);
+//
+//    $result = \Bitrix\Main\Mail\Event::send(array(
+//        "EVENT_NAME" => "SEND",
+//        "LID" => "s1",
+//        "C_FIELDS" => $arFields,
+//
+//    ));
+//    if ($result->isSuccess()) {
+//        echo "Ссылка на оплату отправлена на адрес $email.";
+//    } else {
+//        $errors = $result->getErrorMessages();
+//        echo "Ошибка при отправке письма: " . implode(", ", $errors);
+//    }
 
-    $eventName = 'SEND_PAYMENT_LINK';
-    $siteId = 's1';
-    $fields = array(
-        'EMAIL' => $email,
-        'PAYMENT_LINK' => $request,
-    );
-
-    $result = Event::send(array(
-        "EVENT_NAME" => $eventName,
-        "LID" => SITE_ID,
-        "C_FIELDS" => $fields,
-    ));
-
-    if ($result->isSuccess()) {
+    $to = $email;
+    $subject = "Ссылка на оплату";
+    $headers = "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    $message = '<html><body>';
+    $message = '
+            <div class="mb-4">
+                <p>Услугу предоставляет сервис <b>&laquo;Payselection&raquo;</b>.</p>
+                <p>Сумма к оплате: </p>
+                <div class="d-flex align-items-center mb-3">
+                    <div class="col-auto pl-0">
+                        <a class="btn btn-lg btn-success pl-4 pr-4" style="border-radius: 32px;" href="' . $paymentURL . '">Оплатить</a>
+                    </div>
+                    <div class="col pr-0">&nbsp;&nbsp;Вы будете перенаправлены на страницу оплаты</div>
+                </div>
+            </div>
+        ';
+    $message .= '</body></html>';
+    if (bxmail($to, $subject, $message, $headers)) {
         echo "Ссылка на оплату отправлена на адрес $email.";
     } else {
-        echo "Ошибка при отправке ссылки на оплату.";
+        echo "Ошибка при отправке письма";
     }
+}
 
-};
 
 ?>
