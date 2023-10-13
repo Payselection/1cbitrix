6c4591083b3e87d1384e5eed6aa7b9a2e849a86a
Alexander.Mulyarchik <alexander.mulyarchik@solbeg.com>
2023-10-13 16:40:51 +0300

    fix: include

--- include.php
+++ include.php
@@ -2,22 +2,15 @@
 use \Bitrix\Main\Localization\Loc;
 
 \Bitrix\Main\Loader::registerAutoLoadClasses("p10102022_p10102022paycode2022", array());
+
 class PayselectionButton {
     public static function OrderDetailAdminContextMenuShow_(&$items){
-        if ($_SERVER['REQUEST_METHOD'] == 'GET' && $GLOBALS['APPLICATION']->GetCurPage() == '/bitrix/admin/sale_order_edit.php' && $_REQUEST['ID'] > 0) {
+        if ($_SERVER['REQUEST_METHOD'] == 'GET' && in_array($GLOBALS['APPLICATION']->GetCurPage(), ['/bitrix/admin/sale_order_edit.php', '/bitrix/admin/sale_order_view.php']) && $_REQUEST['ID'] > 0) {
             $items[] = array(
-                "TEXT"=>"Отправить ссылку",
-                "LINK"=>"javascript:payselection_pay_link(".$_REQUEST['ID'].")",
-                "TITLE"=>"Отправить ссылку",
-                "ICON"=>"adm-btn",
-            );
-        }
-        if ($_SERVER['REQUEST_METHOD'] == 'GET' && $GLOBALS['APPLICATION']->GetCurPage() == '/bitrix/admin/sale_order_view.php' && $_REQUEST['ID'] > 0) {
-            $items[] = array(
-                "TEXT"=>"Отправить ссылку",
-                "LINK"=>"javascript:payselection_pay_link(".$_REQUEST['ID'].")",
-                "TITLE"=>"Отправить ссылку",
-                "ICON"=>"adm-btn",
+                "TEXT" => "Отправить ссылку",
+                "LINK" => "javascript:payselection_pay_link(" . $_REQUEST['ID'] . ")",
+                "TITLE" => "Отправить ссылку",
+                "ICON" => "adm-btn",
             );
         }
     }
