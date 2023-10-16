716d60b2f3f34d6d6a356b4917982c08fb511bef
Alexander.Mulyarchik <alexander.mulyarchik@solbeg.com>
2023-10-16 22:40:37 +0300

    update:translates

--- include.php
+++ include.php
@@ -1,5 +1,6 @@
 <?php
 use \Bitrix\Main\Localization\Loc;
+use \Bitrix\Main\Loader;
 
 \Bitrix\Main\Loader::registerAutoLoadClasses("p10102022_p10102022paycode2022", array());
 
@@ -7,9 +8,9 @@ class PayselectionButton {
     public static function OrderDetailAdminContextMenuShow_(&$items){
         if ($_SERVER['REQUEST_METHOD'] == 'GET' && in_array($GLOBALS['APPLICATION']->GetCurPage(), ['/bitrix/admin/sale_order_edit.php', '/bitrix/admin/sale_order_view.php']) && $_REQUEST['ID'] > 0) {
             $items[] = array(
-                "TEXT" => "Отправить ссылку",
+                "TEXT" => Loc::getMessage("PAYSELECTION_SEND_LINK"),
                 "LINK" => "javascript:payselection_pay_link(" . $_REQUEST['ID'] . ")",
-                "TITLE" => "Отправить ссылку",
+                "TITLE" => Loc::getMessage("PAYSELECTION_SEND_LINK_TITLE"),
                 "ICON" => "adm-btn",
             );
         }
